7eec94d963eb6909b8ab1d29d79949131b6da432
Jonas <hygox@outlook.com>
2018-09-20 18:07:53 +0800

    secend

--- GeneralApi/Controllers/HomeController.cs
+++ GeneralApi/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
+/// <summary>
+/// 主页
+/// </summary>
 public class HomeController : ControllerBase {
 
     public ActionResult Index () {
--- GeneralApi/Controllers/ValuesController.cs
+++ GeneralApi/Controllers/ValuesController.cs
@@ -4,42 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GeneralApi.Controllers
-{
-    [Route("api/[controller]")]
+namespace GeneralApi.Controllers {
+    /// <summary>
+    /// 时候用
+    /// </summary>
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
-    {
+    public class ValuesController : ControllerBase {
+        /// <summary>
+        /// 测试用例
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
+        public ActionResult<IEnumerable<string>> Get () {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
+        /// <summary>
+        /// GET api/values/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet ("{id}")]
+        public ActionResult<string> Get (int id) {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// POST api/values
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult<string> Post([FromBody] string value)
-        {
+        public ActionResult<string> Post ([FromBody] string value) {
             return value;
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        /// <summary>
+        /// PUT api/values/5
+        /// </summary>
+        /// <param name="id">编号</param>
+        /// <param name="value">值</param>
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        /// <summary>
+        /// DELETE api/values/5
+        /// </summary>
+        /// <param name="id">编号</param>
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
     }
 }
